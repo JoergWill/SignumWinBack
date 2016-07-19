299dc9759e51050203c588ca9f468eb20cbfae12
Jörg Will <jw@winback.de>
2016-07-19 14:27:50 +0200

    Batch-Files für Jenkins

--- OrgaBack.Copy.bat
+++ OrgaBack.Copy.bat
@@ -1,4 +1,6 @@
-﻿REM Dateien aus dem Workspace nach Orgasoft-Addin kopieren
+﻿@echo off
+
+Rem Workspace nach Orgasoft\Addin kopieren
 mkdir "C:\ProgramData\OrgaSoft\AddIn" 2>NUL
 xcopy "%WORKSPACE%\WinBackAddin.*" "C:\ProgramData\OrgaSoft\AddIn" /E /C /R /Y
 xcopy "%WORKSPACE%*.dll" "C:\ProgramData\OrgaSoft\AddIn" /E /C /R /Y
@@ -9,4 +11,4 @@ REM 10 Sekunden warten
 ping -n 10 localhost 2> NUL
 
 REM OrgaBack neu starten (aktiviert Addin)
-Start /B "OrgaBack" "C:\Program Files (x86)\Signum\Orgasoft\Signum.Orgasoft.Main.exe" /M:01 /U:SYS /P:admin
+cmd.exe /C OrgaBack.Start.bat
---
+++ OrgaBack.Start.bat
@@ -0,0 +1,3 @@
+﻿@echo off
+REM OrgaBack neu starten (aktiviert Addin)
+Start /B "OrgaBack" "C:\Program Files (x86)\Signum\Orgasoft\Signum.Orgasoft.Main.exe" /M:01 /U:SYS /P:admin
