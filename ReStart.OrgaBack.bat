63856b104dee5f038d3decded9cf7a717705d32c
Jörg Will <jw@winback.de>
2016-07-19 14:06:36 +0200

    ReStart OrgaSoft überarbeitet

--- ReStart.OrgaBack.bat
+++ ReStart.OrgaBack.bat
@@ -1,10 +1,12 @@
 ﻿REM Dateien aus dem Workspace nach Orgasoft-Addin kopieren
-mkdir "C:\ProgramData\OrgaSoft\AddIn"
-xcopy "%WORKSPACE%%JOB_BASE_NAME%.*" "C:\ProgramData\OrgaSoft\AddIn" /E /C /R /Y
+mkdir "C:\ProgramData\OrgaSoft\AddIn" 2>NUL
+xcopy "%WORKSPACE%\WinBackAddin.*" "C:\ProgramData\OrgaSoft\AddIn" /E /C /R /Y
 xcopy "%WORKSPACE%*.dll" "C:\ProgramData\OrgaSoft\AddIn" /E /C /R /Y
 
 REM noch laufende Orgasoft-Instanzen beenden
 Taskkill /IM Signum.Orgasoft.Main.exe
+REM 10 Sekunden warten
+ping -n 10 localhost 2> NUL
 
 REM OrgaBack neu starten (aktiviert Addin)
-"C:\Program Files (x86)\Signum\Orgasoft\Signum.Orgasoft.Main.exe" /M:01 /U:SYS /P:admin
+Start /B "OrgaBack" "C:\Program Files (x86)\Signum\Orgasoft\Signum.Orgasoft.Main.exe" /M:01 /U:SYS /P:admin
