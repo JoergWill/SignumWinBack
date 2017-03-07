731d5cd7bb6b606348250af91886ec4ff7bc56f7
Jörg Will <jw@winback.de>
2017-03-07 14:32:22 +0100

    Projekt WinBackServerTask eingefügt

---
+++ WinBackServerTask/wb_Server.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
+        Me.NotifyIcon = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.MainTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.lblCounter = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'NotifyIcon
+        '
+        Me.NotifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info
+        Me.NotifyIcon.Icon = CType(resources.GetObject("NotifyIcon.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon.Text = "WinBack-ServerTask"
+        Me.NotifyIcon.Visible = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(44, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(112, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "WinBack-Server-Task"
+        '
+        'MainTimer
+        '
+        Me.MainTimer.Enabled = True
+        Me.MainTimer.Interval = 1000
+        '
+        'lblCounter
+        '
+        Me.lblCounter.AutoSize = True
+        Me.lblCounter.Location = New System.Drawing.Point(172, 39)
+        Me.lblCounter.Name = "lblCounter"
+        Me.lblCounter.Size = New System.Drawing.Size(31, 13)
+        Me.lblCounter.TabIndex = 1
+        Me.lblCounter.Text = "0000"
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(592, 454)
+        Me.Controls.Add(Me.lblCounter)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Main"
+        Me.ShowInTaskbar = False
+        Me.Text = "WinBack"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents NotifyIcon As NotifyIcon
+    Friend WithEvents Label1 As Label
+    Friend WithEvents MainTimer As Timer
+    Friend WithEvents lblCounter As Label
+End Class
---
+++ WinBackServerTask/wb_Server.vb
@@ -0,0 +1,23 @@
+﻿Public Class Main
+    Private Counter As Integer
+
+    Private Sub NotifyIcon_DoubleClick(sender As Object, e As EventArgs) Handles NotifyIcon.DoubleClick
+        Me.WindowState = FormWindowState.Normal
+        Me.ShowInTaskbar = True
+    End Sub
+
+    Private Sub NotifyIcon_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon.MouseDoubleClick
+
+    End Sub
+
+    Private Sub Main_SizeChanged(sender As Object, e As EventArgs) Handles MyBase.SizeChanged
+        If Me.WindowState = FormWindowState.Minimized Then
+            Me.ShowInTaskbar = False
+        End If
+    End Sub
+
+    Private Sub MainTimer_Tick(sender As Object, e As EventArgs) Handles MainTimer.Tick
+        Counter = Counter + 1
+        lblCounter.Text = String.Format("Ticks {0:#}", Counter)
+    End Sub
+End Class
